340fbaf337ac789205b2ee5807366bb1d97ef236
tberic <4753153+tberic@users.noreply.github.com>
2022-04-03 23:24:54 +0200

    solved 4:1

---
+++ 2019/4/1.js
@@ -0,0 +1,17 @@
+function valid(x) {
+    let s = x.toString();
+    if (s[0] != s[1] && s[1] != s[2] && s[2] != s[3] && s[3] != s[4] && s[4] != s[5])
+        return false;
+    return (s[0] <= s[1] && s[1] <= s[2] && s[2] <= s[3] && s[3] <= s[4] && s[4] <= s[5]);
+}
+
+const start = 246540;
+const end = 787419;
+
+let count = 0;
+for (let x = start; x <= end; ++x) {
+    if ( valid(x) )
+        count++;
+}
+
+console.log(count);
