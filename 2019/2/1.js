dd4adf90eeeaf7e1fd34c1f77768c4b3a1c1d3ec
tberic <4753153+tberic@users.noreply.github.com>
2022-04-03 13:13:33 +0200

    AoC 2019

--- 2019/2/1.js
+++ 2019/2/1.js
@@ -4,5 +4,12 @@ data[1] = 12;
 data[2] = 2;
 
 for (let i = 0; i < data.length; i += 4) {
-    
+    if (data[i] == 1)
+        data[data[i+3]] = data[data[i+1]] + data[data[i+2]];
+    else if (data[i] == 2)
+        data[data[i+3]] = data[data[i+1]] * data[data[i+2]];
+    else if (data[i] == 99)
+        break;    
 }
+
+console.log(data[0]);
