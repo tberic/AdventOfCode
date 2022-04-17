36be314422b9ac49a853cfb936d2c32f23dcb518
tberic <4753153+tberic@users.noreply.github.com>
2022-04-17 10:45:34 +0200

    solved day 13

--- 2019/13/2.js
+++ 2019/13/2.js
@@ -1,6 +1,41 @@
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function draw() {
+    function convert(c) {
+        if (c == 0) return '.';
+        if (c == 1) return 'X';
+        if (c == 2) return '#';
+        if (c == 3) return '=';
+        if (c == 4) return 'o';
+    }
+    
+    console.clear();
+
+    let s = "";
+    for (let y = 0; y < 22; ++y) {
+        s = "";
+        for (let x = 0; x < SIZE; ++x) 
+            s += convert(grid[x][y]);
+        console.log(s);
+    }
+    
+    await delay(10);
+}
+
+function sign(x) {
+    if (x == 0) return 0;
+    return x/Math.abs(x);
+}
+
+
 let relativeBase = 0;
 
-function runCode(data=dataMap) {
+let ballx, bally;
+let paddlex, paddley;
+const SIZE = 40; //Math.max(...inst)+1;
+let grid = new Array(SIZE).fill(0).map(() => new Array(SIZE).fill(0));
+
+async function runCode(data=dataMap) {
     let i = 0;
     let outputs = [];
 
@@ -21,15 +56,36 @@ function runCode(data=dataMap) {
             i += 4;
         }
         else if (op == 3) {
-            console.log('INPUT DETECTED');
-            //data.set( pos2(data, i+1, c),  );
+            //console.log('INPUT DETECTED@' + i);
+            data.set( pos2(data, i+1, c), sign(ballx - paddlex) );            
+            //await draw();
             i += 2;
         }
         else if (op == 4) {
             //console.log(data.get( pos(data, i+1, c) ));
             let out = data.get( pos(data, i+1, c) );
-            outputs.push(out);            
+            outputs.push(out);
+            if (outputs.length == 3) {
+                //console.log(outputs);
+                if (outputs[0] == -1 && outputs[1] == 0)
+                    console.log(outputs[2]);
+                else
+                    grid[outputs[0]][outputs[1]] = outputs[2];
+
+                if (outputs[2] == 4) {
+                    ballx = outputs[0];
+                    bally = outputs[1];
+                }
+                if (outputs[2] == 3) {
+                    paddlex = outputs[0];
+                    paddley = outputs[1];
+                }
+
+                outputs = [];
+            }
             i += 2;
+
+            //draw();
         }
         else if (op == 5) {
             if (data.get( pos(data, i+1, c) ))
@@ -84,6 +140,7 @@ function pos2(data, i, code) {
 }
 
 let fin = require("fs");
+const { finished } = require("stream");
 const origData = fin.readFileSync('input.txt', 'utf8').toString().split(',').filter(x => x).map(x => parseInt(x));
 
 let dataMap = new Map();
@@ -91,24 +148,5 @@ for (let i = 0; i < origData.length; ++i)
     dataMap.set(i, origData[i]);
 dataMap.set(-1, 0);
 
-let inst = runCode();
-
-const SIZE = Math.max(...inst)+1;
-let grid = new Array(SIZE).fill(0).map(() => new Array(SIZE).fill(0));
-
-//console.log( Math.max(...inst) + " " + Math.min(...inst) );
-
-let i = 0;
-while (i < inst.length) {
-    grid[inst[i]][inst[i+1]] = inst[i+2];
-
-    i += 3;
-}
-
-let count = 0;
-for (let x = 0; x < SIZE; ++x)
-    for (let y = 0; y < SIZE; ++y)
-        if (grid[x][y] == 2)
-            count++;
-
-console.log(count);
+dataMap.set(0, 2);
+runCode();
