efc06d1c2304240b660c37aaf3487f4519f5ff07
tberic <4753153+tberic@users.noreply.github.com>
2022-04-27 16:57:32 +0200

    solved day 25

---
+++ 2019/25/1.js
@@ -0,0 +1,122 @@
+let relativeBase = 0;
+
+function runCode(input, i = 0) {
+    let outputs = [];
+
+//    let data = new Map(dataIn);
+
+    while (data.get(i) != 99) {
+        let a = Math.floor(data.get(i) / 10000);
+        let b = Math.floor(data.get(i) / 1000) % 10;
+        let c = Math.floor(data.get(i) / 100) % 10;
+        let op = data.get(i) % 100;
+
+        //console.log(i + ": " + op + "(" + a+b+c + ") "  + relativeBase);        
+
+        if (op == 1) {
+            data.set( pos2(data, i+3, a), data.get( pos(data, i+1, c) ) + data.get( pos(data, i+2, b) ) );
+            i += 4;
+        }
+        else if (op == 2) {
+            data.set( pos2(data, i+3, a), data.get( pos(data, i+1, c) ) * data.get( pos(data, i+2, b) ) );
+            i += 4;
+        }
+        else if (op == 3) {
+            if (input.length > 0) 
+                data.set( pos2(data, i+1, c), input.shift() );
+            else
+                return [i, outputs];
+            i += 2;
+        }
+        else if (op == 4) {
+            //console.log(data.get( pos(data, i+1, c) ));
+            let out = data.get( pos(data, i+1, c) );
+            outputs.push(out);
+            i += 2;
+        }
+        else if (op == 5) {
+            if (data.get( pos(data, i+1, c) ))
+                i = data.get( pos(data, i+2, b) );
+            else
+                i += 3;
+        }
+        else if (op == 6) {
+            if (!data.get( pos(data, i+1, c) ))
+                i = data.get( pos(data, i+2, b) );
+            else
+                i += 3;
+        }
+        else if (op == 7) {
+            data.set( pos2(data, i+3, a), +(data.get( pos(data, i+1, c) ) < data.get( pos(data, i+2, b) )) );
+            i += 4;
+        }
+        else if (op == 8) {
+            data.set( pos2(data, i+3, a), +(data.get( pos(data, i+1, c) ) == data.get( pos(data, i+2, b) )) );
+            i += 4;
+        }
+        else if (op == 9) {
+            relativeBase += data.get( pos(data, i+1, c) );            
+            i += 2;
+        }
+        else {
+            console.log("ERROR");
+            break;
+        }
+    }
+
+    return [0, outputs];
+}
+
+
+function pos(data, i, code) {
+    if (code == 0)
+        return (data.has(data.get(i))) ? data.get(i) : -1;
+    if (code == 1)
+        return i;
+    if (code == 2)
+        return (data.has( data.get(i) + relativeBase )) ? data.get(i) + relativeBase : -1;
+}
+
+function pos2(data, i, code) {
+    if (code == 0)
+        return data.get(i);
+    if (code == 1)
+        return i;
+    if (code == 2)
+        return data.get(i) + relativeBase;
+}
+
+let fs = require("fs");
+const origData = fs.readFileSync('input.txt', 'utf8').toString().split(',').filter(x => x).map(x => parseInt(x));
+
+let data = new Map();
+for (let i = 0; i < origData.length; ++i)
+    data.set(i, origData[i]);
+data.set(-1, 0);
+
+const prompt = require('prompt-sync')();
+let command = "inv";
+
+let i = 0, output = [];
+while (true) {
+    [i, output] = runCode( convertInput( command+"\n" ), i );
+
+    let text = String.fromCharCode(...output);
+    console.log( text );
+
+    if (i == 0)
+        break;
+
+    command = prompt('');
+    if (command == "quit")
+        break;
+}
+
+
+function convertInput(s) {
+    let ascii = [];
+    for (let i = 0; i < s.length; ++i)
+        ascii.push( s.charCodeAt(i) );
+
+    return ascii;
+}
