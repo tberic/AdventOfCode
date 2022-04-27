2870e1783f92f834f0baed95b946ecd7c5912d9f
tberic <4753153+tberic@users.noreply.github.com>
2022-04-27 07:59:22 +0200

    22:1 solution without the array

--- 2019/22/1.js
+++ 2019/22/1.js
@@ -1,42 +1,30 @@
 let fs = require("fs");
 const lines = fs.readFileSync('input.txt', 'utf8').toString().split('\n').filter( x => x );
 
-const SIZE = 10007;
-let cards = new Array(SIZE);
-for (let i = 0; i < SIZE; ++i)
-    cards[i] = i;
+const M = 10007;
+let pos = 2019;
 
 for (let line of lines) {
     if (line.split(' ')[0] == "deal" && line.split(' ')[1] == "with")
-        dealWithIncrement( +line.split(' ')[3] );
+        pos = dealWithIncrement( pos, +line.split(' ')[3] );
     else if (line.split(' ')[0] == "deal" && line.split(' ')[1] == "into")
-        dealIntoNewStack( );
+        pos = dealIntoNewStack( pos );
     else if (line.split(' ')[0] == "cut")
-        cutCards( +line.split(' ')[1] );
+        pos = cutCards( pos, +line.split(' ')[1] );
 }
 
-console.log(cards.indexOf(2019));
+console.log(pos);
 
 
 
-function dealIntoNewStack() {
-    cards.reverse();
+function dealIntoNewStack(x) {
+    return (M - x - 1);
 }
 
-function cutCards(N) {
-    if (N < 0)
-        cutCards(SIZE+N);
-    else {
-        cards = cards.slice( N, SIZE ).concat( cards.slice( 0, N ) );
-    }
+function cutCards(x, N) {
+    return ( x + M - N ) % M;
 }
 
-function dealWithIncrement(N) {
-    let cards2 = new Array(SIZE);
-    let pos = 0;
-    for (let i = 0; i < SIZE; ++i) {
-        cards2[ pos ] = cards[i];
-        pos = (pos + N) % SIZE;
-    }
-    cards = [...cards2];
+function dealWithIncrement(x, N) {
+    return x*N % M;
 }
